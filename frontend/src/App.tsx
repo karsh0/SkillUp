ff454c7972b41fca16e6819c6de2445cf51456af
Karan <karansharma192004@gmail.com>
2024-12-09 22:28:26 +0530

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='h-screen w-screen flex text-center justify-center items-center text-4xl font-bold text-white bg-slate-700'>
+      SkillUp
+    </div>
+  )
+}
+
+export default App
